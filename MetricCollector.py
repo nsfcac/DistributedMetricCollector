19228cec929b72c664846061c4bcfd89e7bfde50
ghazanfarttu <ghazanfar.ali@ttu.edu>
2020-04-08 17:36:23 -0500

    jobs

--- MetricCollector.py
+++ MetricCollector.py
@@ -1782,7 +1782,7 @@ def main():
     # hostList = ['10.101.10.25']
     # For the purpose of this testing, I have excluded the HPCJob metric:
     # checkList = ['SystemHealth','BMCHealth','Thermal','Power']
-    checkList = ['BMCHealth','SystemHealth','Thermal','Power','MEMPWR','CPUPWR']
+    #checkList = ['BMCHealth','SystemHealth','Thermal','Power','MEMPWR','CPUPWR']
     
     '''
     # Checks are iterated 100 times across the TTU HPCC Quanah cluster (467 nodes)
