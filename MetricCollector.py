d9d1ee4d69b1c0b54aba06d7b979b623c8f75c8c
ghazanfarttu <ghazanfar.ali@ttu.edu>
2020-04-08 17:27:42 -0500

    cluster

--- MetricCollector.py
+++ MetricCollector.py
@@ -1779,7 +1779,7 @@ def main():
     #REMOVEME
     checkList = ['BMCHealth','SystemHealth','HPCJob','Thermal','Power','MEMPWR','CPUPWR']
 
-    hostList = ['10.101.10.25']
+    # hostList = ['10.101.10.25']
     # For the purpose of this testing, I have excluded the HPCJob metric:
     # checkList = ['SystemHealth','BMCHealth','Thermal','Power']
     checkList = ['BMCHealth','SystemHealth','Thermal','Power','MEMPWR','CPUPWR']
