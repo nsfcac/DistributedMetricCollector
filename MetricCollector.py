817b1d271a183104e6781225f691a3daefa2c5c2
ghazanfarttu <ghazanfar.ali@ttu.edu>
2020-04-10 12:17:22 -0500

    mempwr and cpupwr

--- MetricCollector.py
+++ MetricCollector.py
@@ -1966,7 +1966,7 @@ def main():
     # hostList = ['10.101.10.25']
     # For the purpose of this testing, I have excluded the HPCJob metric:
     # checkList = ['SystemHealth','BMCHealth','Thermal','Power']
-    checkList = ['Power','MEMPWR','CPUPWR']
+    checkList = ['MEMPWR','CPUPWR']
     
     '''
     # Checks are iterated 100 times across the TTU HPCC Quanah cluster (467 nodes)
